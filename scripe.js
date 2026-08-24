66da933903c655fdfd463ff66f407bdc05713e9f
manotti22 <97608984+manotti22@users.noreply.github.com>
2026-08-24 19:30:19 +0100

    Update scripe.js

--- scripe.js
+++ scripe.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+  // Aktuelles Jahr automatisch anzeigen
+  const footer = document.querySelector("footer");
+
+  if (footer) {
+    footer.innerHTML = footer.innerHTML.replace(
+      "© 2026",
+      "© " + new Date().getFullYear()
+    );
+  }
+
+});
